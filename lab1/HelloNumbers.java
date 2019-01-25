c744137cf9a9b16f2f97d642eae17aea6224512a
Adithya Seshan <adithya.seshan21@berkeley.edu>
2019-01-25 12:24:14 -0800

    Edited numbers

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -3,7 +3,7 @@ public class HelloNumbers {
     public static void main(String[] args) {
         int x = 0;
         int y = 0;
-        while (x < 9) {
+        while (x < 10) {
             System.out.print(y + " ");
             
             x = x + 1;
