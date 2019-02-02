8c52efb4ee0b411bfc604818b8891b0af3d6b69e
Adithya Seshan <adithya.seshan21@berkeley.edu>
2019-02-01 17:01:25 -0800

    Final

--- proj0/Body.java
+++ proj0/Body.java
@@ -2,6 +2,7 @@ public class Body {
 
 	public double xxPos, yyPos, xxVel, yyVel, mass;
 	public String imgFileName; 
+	public final static double g = 6.67e-11;
 
 	public Body(double xP, double yP, double xV, double yV, double m, String img) {
 		xxPos = xP;
@@ -19,7 +20,69 @@ public class Body {
 		yyVel = p.yyVel;
 		mass = p.mass;
 		imgFileName = p.imgFileName;
-	}
-	
+	} 
 
+	public double calcDistance(Body b) {
+
+		double dx = this.xxPos - b.xxPos;
+		double dy = this.yyPos - b.yyPos;
+		return Math.sqrt(dx * dx + dy* dy);
+	}
+
+	public double calcForceExertedBy(Body b) {
+
+		return (g * this.mass * b.mass) / (this.calcDistance(b) * this.calcDistance(b));
+	}
+
+	public double calcForceExertedByX(Body b) {
+		double first = Math.max(this.xxPos, b.xxPos);
+		double sec = Math.min(this.xxPos, b.xxPos);
+		double dx = first - sec;
+		return (this.calcForceExertedBy(b) * dx)/this.calcDistance(b);
+	}
+	public double calcForceExertedByY(Body b) {
+		double first = Math.max(this.yyPos, b.yyPos);
+		double sec = Math.min(this.yyPos, b.yyPos);
+		double dy = first - sec;
+		return (this.calcForceExertedBy(b) * dy)/this.calcDistance(b);
+	}
+	public double calcNetForceExertedByX(Body[] b){
+		double sum = 0;
+		for (Body x : b){
+
+			if (this.equals(x)){
+				continue;
+			}
+			sum += this.calcForceExertedByX(x);
+		}
+		return sum;
+
+	}
+	public double calcNetForceExertedByY(Body[] b){
+		double sum = 0;
+		for(Body y : b) {
+			if (this.equals(y)){
+				continue;
+			}
+			sum += this.calcForceExertedByY(y); 
+		}
+		return sum;
+
+	}
+
+	public void update(double dt, double fX, double fY) {
+
+		xxVel = xxVel + dt * (fX/mass);
+		yyVel = yyVel + dt * (fY/mass);
+
+		xxPos = xxPos + xxVel * dt;
+		yyPos = yyPos + yyVel * dt;
+
+	}
+	public void draw() {
+		StdDraw.picture(xxPos, yyPos, "images/"+imgFileName);
+	}
 }
+
+
+
