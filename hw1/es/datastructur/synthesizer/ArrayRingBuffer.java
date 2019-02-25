1614c8ba97dc25f0fcbbd25e4b1cda7b463ca673
Adithya Seshan <adithya.seshan21@berkeley.edu>
2019-02-24 19:10:20 -0800

    plz

--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -1,9 +1,6 @@
 package es.datastructur.synthesizer;
-import java.util.Iterator;
+// import java.util.Iterator;
 
-//TODO: Make sure to that this class and all of its methods are public
-//TODO: Make sure to add the override tag for all overridden methods
-//TODO: Make sure to make this class implement BoundedQueue<T>
 
 public class ArrayRingBuffer<T> implements BoundedQueue<T> {
     /* Index for the next dequeue or peek. */
@@ -21,8 +18,7 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T> {
      * Create a new ArrayRingBuffer with the given capacity.
      */
     public ArrayRingBuffer(int capacity) {
-        // TODO: Create new array with capacity elements.
-        //       first, last, and fillCount should all be set to 0.
+
 
         rb = (T[]) new Object[capacity];
         first = 0;
@@ -35,9 +31,8 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T> {
      * Adds x to the end of the ring buffer. If there is no room, then
      * throw new RuntimeException("Ring buffer overflow").
      */
+    @Override
     public void enqueue(T x) {
-        // TODO: Enqueue the item. Don't forget to increase fillCount and update
-        //       last.
 
         rb[last] = x;
         last = (last + 1) % cap;
@@ -48,9 +43,9 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T> {
      * Dequeue oldest item in the ring buffer. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and
-        //       update first.
+
         T item = rb[first];
         rb[first] = null;
         first = (first + 1) % cap;
@@ -62,20 +57,21 @@ public class ArrayRingBuffer<T> implements BoundedQueue<T> {
      * Return oldest item, but don't remove it. If the buffer is empty, then
      * throw new RuntimeException("Ring buffer underflow").
      */
+    @Override
     public T peek() {
-        // TODO: Return the first item. None of your instance variables should
-        //       change.
+
         return rb[first];
     }
 
+    @Override
     public int capacity() {
         return cap;
     }
 
+    @Override
     public int fillCount() {
         return fillCount;
     }
-    // TODO: When you get to part 4, implement the needed code to support
-    //       iteration and equals.
 }
-    // TODO: Remove all comments that say TODO when you're done.
+
+
