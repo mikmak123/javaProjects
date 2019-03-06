7aac4d7d67ac1ac2f9fde73ec7e23ce2fb5c4631
Adithya Seshan <adithya.seshan21@berkeley.edu>
2019-03-06 06:13:57 -0800

    cmon cuh

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -6,48 +6,87 @@ public class Percolation {
 
     private boolean[][] perc;
     private int openSites;
+    private int top;
+    private int bottom;
+    private int len;
     WeightedQuickUnionUF track;
+
 
     public Percolation(int N) {
         perc = new boolean[N][N];
-        track = new WeightedQuickUnionUF((N + 2) * (N + 2));
+        track = new WeightedQuickUnionUF(N * N + 2);
         openSites = 0;
+        top = N * N + 1;
+        bottom = N * N + 2;
+        len = N;
+    }
+
+    private int convert(int i, int j) {
+        return i * perc.length + j;
     }
 
     public void open(int row, int col) {
         if (row >= perc.length || col >= perc.length || row < 0 || col < 0) {
             throw new IndexOutOfBoundsException("Your row or column is out of range");
         }
-        perc[row][col] = true;
-        track.union((2 + perc.length) * (row + 1) + col + 2,
-                (2 + perc.length) * (row - 1 + 1) + col + 2);
-        track.union((2 + perc.length) * (row + 1) + col + 2,
-                (2 + perc.length) * (row + 1 + 1) + col + 2);
-        track.union((2 + perc.length) * (row + 1) + col + 2,
-                (2 + perc.length) * (row + 1) + col - 1 + 2);
-        track.union((2 + perc.length) * (row + 1) + col + 2,
-                (2 + perc.length) * (row + 1) + col + 1 + 2);
-        openSites++;
+        if (perc[row][col] == true) {
+            return;
+        } else {
+            perc[row][col] = true;
+            openSites++;
+            int space = convert(row, col);
+            int one = convert(row - 1, col);
+            int two = convert(row + 1, col);
+            int three = convert(row, col + 1);
+            int four = convert(row, col - 1);
+
+            for (int i = 0; i < len; i++) {
+                if (convert(row, col) == i) {
+                    track.union(convert(row, col), len + 1);
+                }
+            }
+            for (int i = len * len - len; i < len * len; i++) {
+                if (convert(row, col) == i) {
+                    track.union(convert(row, col), bottom);
+                }
+            }
+
+            for (int i = len; i < len * (len - 1); i += len) {
+
+                if (space == i) {
+                    track.union(space, two);
+                    track.union(space, four);
+                    track.union(space, three;
+                }
+            }
+            for (int i = len + 4; i < len * len - 1; i += len) {
+                if (space == i) {
+                    track.union(space, one);
+                    track.union(space, four);
+                    track.union(space, three;
+                } else {
+                    track.union(space, one);
+                    track.union(space, two);
+                    track.union(space, four);
+                    track.union(space, three;
+                }
+            }
+        }
     }
 
     public boolean isOpen(int row, int col) {
         if (row >= perc.length || col >= perc.length || row < 0 || col < 0) {
             throw new IndexOutOfBoundsException("Your row or column is out of range");
         }
-
         return perc[row][col];
     }
+
 
     public boolean isFull(int row, int col) {
         if (row >= perc.length || col >= perc.length || row < 0 || col < 0) {
             throw new IndexOutOfBoundsException("Your row or column is out of range");
         }
-        for (int i = 1; i <= perc.length; i++) {
-            if (track.connected((2 + perc.length) * (row + 1) + col + 2, i)) {
-                return true;
-            }
-        }
-        return false;
+        return track.connected(convert(row, col), top);
     }
 
     public int numberOfOpenSites() {
@@ -56,12 +95,7 @@ public class Percolation {
 
     public boolean percolates() {
 
-        for (int i = 0; i < perc.length; i++) {
-            if (isFull(perc.length - 1, i)) {
-                return true;
-            }
-        }
-        return false;
+        return track.connected(top, bottom);
     }
 
     public static void main(String[] args) {
